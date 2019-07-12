bad5013bdec1d63b3f600bedbe08c67d68a7fe0a
tba <analista2@tbadigital.com.br>
2019-07-12 14:54:25 -0300

    ˜Primeiro Commit

---
+++ www/resources/scenarios/function-jim-links1562942759483.js
@@ -0,0 +1,17 @@
+(function(window, undefined) {
+
+  var jimLinks = {
+    "373db573-4daa-4a97-b4f6-5f8c37e0b9e5" : {
+      "Button_1" : [
+        "d12245cc-1680-458d-89dd-4f0d7fb22724"
+      ]
+    },
+    "d12245cc-1680-458d-89dd-4f0d7fb22724" : {
+      "Button_1" : [
+        "373db573-4daa-4a97-b4f6-5f8c37e0b9e5"
+      ]
+    }    
+  }
+
+  window.jimLinks = jimLinks;
+})(window);
